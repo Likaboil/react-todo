4c05973c6aaa5b08fb6d8e38762a666d976fd10c
Liliya Sagitova <guverboil@gmail.com>
2021-01-15 17:10:45 +0300

    Breaks the mockup into elements

--- src/index.js
+++ src/index.js
@@ -1,30 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (
-  <div>
+const AppHeader = () => {
+  return (
     <div>
       <h1>My Todo List</h1>
       <h2>3 more to do, 0 done</h2>
-    </div>
+  </div>
+  );
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const SearchBtn = () => {
+  return (
     <div>
-      <input placeholder="search" />
-      <div>
-        <button type="button">All</button>
-        <button type="button">Active</button>
-        <button type="button">Done</button>
-      </div>
+      <button type="button">All</button>
+      <button type="button">Active</button>
+      <button type="button">Done</button>
     </div>
+  );
+};
+
+const TaskList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Cook dinner</li>
       <li>Build Awesome App</li>
-    </ul>
+   </ul>
+  );
+};
+
+const AddTaskForm = () => {
+  return (
     <form>
       <input type="text"
-             placeholder="What needs to be done?" />
+              placeholder="What needs to be done?" />
       <button type="submit">Add</button>
     </form>
+  );
+};
+
+const App = (
+  <div>
+    <AppHeader />
+    <div>
+      <SearchPanel />
+      <SearchBtn />
+    </div>
+    <TaskList />
+    <AddTaskForm />
   </div>
 );
 
