9335c10c297d8f875a3438988806f6fe998ff304
Liliya Sagitova <guverboil@gmail.com>
2021-01-15 17:44:07 +0300

    Moves element TaskList to components, adds imports at main index.js

---
+++ src/components/task-list/task-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TaskList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Cook dinner</li>
+      <li>Build Awesome App</li>
+   </ul>
+  );
+};
+
+export default TaskList;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import ReactDOM from 'react-dom';
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import SearchBtn from './components/search-btn';
-
-const TaskList = () => {
-  return (
-    <ul>
-      <li>Learn React</li>
-      <li>Cook dinner</li>
-      <li>Build Awesome App</li>
-   </ul>
-  );
-};
+import TaskList from './components/task-list';
 
 const AddTaskForm = () => {
   return (
