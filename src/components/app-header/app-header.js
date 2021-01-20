80eb1c792356ee04f3b5eaf059a9f5f2c8065f46
Liliya Sagitova <guverboil@gmail.com>
2021-01-20 11:10:42 +0300

    Adds styles to component AppHeader

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './app-header.css';
 
 const AppHeader = () => {
   return (
-    <div>
+    <div className="app-header d-flex">
       <h1>My Todo List</h1>
       <h2>3 more to do, 0 done</h2>
   </div>
