42e18e05fe35f5fc468623fe47d990db94840e2a
Liliya Sagitova <guverboil@gmail.com>
2021-01-20 13:16:01 +0300

    feat: Adds styles to component TaskList

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './task-list.css';
 
 const TaskList = () => {
   return (
-    <ul>
-      <li>Learn React</li>
-      <li>Cook dinner</li>
-      <li>Build Awesome App</li>
+    <ul className="task-list list-group">
+      <li className="list-group-item">Learn React</li>
+      <li className="list-group-item">Cook dinner</li>
+      <li className="list-group-item">Build Awesome App</li>
    </ul>
   );
 };
