b8ee38780dfef6b7acb50d58121674ab1583f0d0
Liliya Sagitova <guverboil@gmail.com>
2021-01-22 15:03:38 +0300

    refactor: Converts from functional to class AddTaskForm

--- src/components/add-task-form/add-task-form.js
+++ src/components/add-task-form/add-task-form.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './add-task-form.css';
 
-const AddTaskForm = () => {
-  return (
-    <form className="add-panel d-flex">
-      <input className="form-control new-task-label"
-              type="text"
-              placeholder="What needs to be done?" />
-      <button className="btn btn-outline-success"
-              type="submit">Add</button>
-    </form>
-  );
-};
-
-export default AddTaskForm;
+export default class AddTaskForm extends Component {
+  render() {
+    return (
+      <form className="add-panel d-flex">
+        <input className="form-control new-task-label"
+                type="text"
+                placeholder="What needs to be done?" />
+        <button className="btn btn-outline-success"
+                type="submit">Add</button>
+      </form>
+    );
+  }
+}
