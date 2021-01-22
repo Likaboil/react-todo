c6a69734c73efc63fd2930616242bf489bb692c8
Liliya Sagitova <guverboil@gmail.com>
2021-01-22 11:24:31 +0300

    fix: Moves element TodoListItem to components, adds styles and imports

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './todo-list-item.css';
+
+const TodoListItem = () => {
+  return <span className="todo-list-item-label">Learn React</span>
+};
+
+export default TodoListItem;
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './todo-list.css';
 
-const TodoListItem = (
-  <span>Learn React</span>
-)
+import TodoListItem from '../todo-list-item';
 
 const TodoList = () => {
   return (
     <ul className="todo-list list-group">
-      <li className="list-group-item">{ TodoListItem }</li>
-      <li className="list-group-item">{ TodoListItem }</li>
-      <li className="list-group-item">{ TodoListItem }</li>
+      <li className="list-group-item"><TodoListItem /></li>
+      <li className="list-group-item"><TodoListItem /></li>
+      <li className="list-group-item"><TodoListItem /></li>
    </ul>
   );
 };
