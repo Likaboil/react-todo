3f6390bef0e08841fd9ac63f7f3f27a812f164f4
Liliya Sagitova <guverboil@gmail.com>
2021-01-22 15:04:39 +0300

    refactor: Converts from functional to class App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 
 import AppHeader from '../app-header';
@@ -8,25 +8,25 @@ import TodoList from '../todo-list';
 import AddTaskForm from '../add-task-form';
 
 
-const App = () => {
+export default class App extends Component {
 
-  const todoListDefault = [
-    {label: "Learn React", important: true, key: 1 },
-    {label: "Cook dinner", important: false, key: 2 },
-    {label: "Build Awesome App", important: true, key: 3 },
-  ]
+  render() {
+    const todoListDefault = [
+      {label: "Learn React", important: true, key: 1 },
+      {label: "Cook dinner", important: false, key: 2 },
+      {label: "Build Awesome App", important: true, key: 3 },
+    ]
 
-  return (
-    <div className="todo-app">
-      <AppHeader />
-      <div className="search-panel d-flex">
-        <SearchPanel />
-        <Filters />
+    return (
+      <div className="todo-app">
+        <AppHeader />
+        <div className="search-panel d-flex">
+          <SearchPanel />
+          <Filters />
+        </div>
+        <TodoList todoListProps = {todoListDefault} />
+        <AddTaskForm />
       </div>
-      <TodoList todoListProps = {todoListDefault} />
-      <AddTaskForm />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
