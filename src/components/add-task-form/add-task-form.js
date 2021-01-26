975a991bc1ca8ddd7baa86a11d2f5e4781d93649
Liliya Sagitova <guverboil@gmail.com>
2021-01-26 19:20:29 +0300

    feat: Adds eventListener for add-task input at AddTaskForm

--- src/components/add-task-form/add-task-form.js
+++ src/components/add-task-form/add-task-form.js
@@ -3,13 +3,18 @@ import './add-task-form.css';
 
 export default class AddTaskForm extends Component {
 
+  onLabelChange=()=> {
+    console.log('Change input value');
+  }
+
   render() {
 
     return (
       <form className="add-panel d-flex">
         <input className="form-control new-task-label"
                 type="text"
-                placeholder="What needs to be done?" />
+                placeholder="What needs to be done?"
+                onChange={this.onLabelChange} />
         <button className="btn btn-outline-success"
                 type="button"
                 onClick={()=> this.props.onTaskAdded(`task for adding to list`)}>
