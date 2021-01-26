266c03eea28f5f325151f76a2702eaec02fec118
Liliya Sagitova <guverboil@gmail.com>
2021-01-26 18:58:36 +0300

    refactor: Converts from class to functional component TodoListItem

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './todo-list-item.css';
 
+const TodoListItem = ({
+  label, OnDeleted,
+  onToggleDone, onToggleImportant,
+  done, important}) => {
 
-export default class TodoListItem extends Component {
+  let classNames = "todo-list-item";
 
-  render() {
-    const {label, OnDeleted, onToggleDone, onToggleImportant, done, important} = this.props;
+  if (done) {
+    classNames += ' done';
+  };
 
-    let classNames = "todo-list-item";
+  if (important) {
+    classNames += ' important'
+  };
 
-    if(done) {
-      classNames += ' done';
-    }
-
-    if(important) {
-      classNames += ' important'
-    }
-
-    return (
-      <span className={classNames}>
-        <span className="todo-list-item-label"
-              onClick={onToggleDone}>
-          {label}
-        </span>
-        <button className="btn btn-outline-danger btn-sm float-right"
-                type="button"
-                onClick={OnDeleted}>
-          <i className="fa fa-trash" />
-        </button>
-        <button className="btn btn-outline-success btn-sm float-right"
-                type="button"
-                onClick={onToggleImportant}>
-          <i className="fa fa-exclamation" />
-        </button>
+  return (
+    <span className={classNames}>
+      <span className="todo-list-item-label"
+            onClick={onToggleDone}>
+        {label}
       </span>
-    );
-  }
+      <button className="btn btn-outline-danger btn-sm float-right"
+              type="button"
+              onClick={OnDeleted}>
+        <i className="fa fa-trash" />
+      </button>
+      <button className="btn btn-outline-success btn-sm float-right"
+              type="button"
+              onClick={onToggleImportant}>
+        <i className="fa fa-exclamation" />
+      </button>
+    </span>
+  );
 }
+
+export default TodoListItem
