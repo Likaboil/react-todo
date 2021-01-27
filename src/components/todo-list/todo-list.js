a1f8c3c9c4f4370ec94a3795c4e29dfe8def7ff0
Liliya Sagitova <guverboil@gmail.com>
2021-01-27 13:32:30 +0300

    refactor: Code Review for TodoList

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,25 +2,26 @@ import React from 'react';
 import './todo-list.css';
 import TodoListItem from '../todo-list-item';
 
-const TodoList = ({todoListProps, onDeleted, onToggleImportant, onToggleDone}) => {
+const TodoList = ({ todoListProps, onDeleted, onToggleImportant, onToggleDone }) => {
 
   const items = todoListProps.map((item) => {
 
-    const {key, ...itemProps} = item;
+    const { key, ...itemProps } = item;
 
     return (
-      <li className="list-group-item" key={key}>
-        <TodoListItem {...itemProps}
-        OnDeleted={()=> onDeleted(key)}
-        onToggleImportant={() => onToggleImportant(key)}
-        onToggleDone={() => onToggleDone(key)} />
+      <li className = "list-group-item" key = { key }>
+        <TodoListItem
+          { ...itemProps }
+          OnDeleted = {() => onDeleted(key)}
+          onToggleImportant = {() => onToggleImportant(key)}
+          onToggleDone = {() => onToggleDone(key)} />
       </li>
     );
   });
 
   return (
-    <ul className="todo-list list-group">
-      {items}
+    <ul className = "todo-list list-group">
+      { items }
    </ul>
   );
 };
