b3e73c79829cf015ae0338438ffa9d0867dd2c96
Liliya Sagitova <guverboil@gmail.com>
2021-01-27 13:29:36 +0300

    refactor: Code Review for SearchPanel

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -3,24 +3,23 @@ import './search-panel.css';
 
 export default class SearchPanel extends Component {
 
-  state={
-    label:``
+  state = {
+    label: ``
   }
 
   onLabelChange = (evt) => {
 
     const label = evt.target.value
 
-    this.setState({label});
+    this.setState({ label });
     this.props.onSearchChange(label);
   }
 
   render() {
-    return <input className="form-control search-input"
-                  placeholder="search"
-                  type="text"
-                  value={this.state.label}
-                  onChange={this.onLabelChange}
-                  />;
+
+    return <input className = "form-control search-input"
+                  placeholder = "search" type = "text"
+                  value = { this.state.label }
+                  onChange = { this.onLabelChange } />;
   }
 };
