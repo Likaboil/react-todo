456556b901b3151184fa84a75c8e21c332610cc3
Liliya Sagitova <guverboil@gmail.com>
2021-01-27 13:27:41 +0300

    refactor: Code Review for AppHeader

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './app-header.css';
 
-const AppHeader = ({active, done}) => {
+const AppHeader = ({ active, done }) => {
+
   return (
-    <div className="app-header d-flex">
+    <div className = "app-header d-flex">
       <h1>My Todo List</h1>
-      <h2>{active} more to do, {done} done</h2>
+      <h2>{ active } more to do, { done } done</h2>
   </div>
   );
 };
