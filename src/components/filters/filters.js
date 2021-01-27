5ee7c5dfd3a70dd74b534ccebf5a805e6928038f
Liliya Sagitova <guverboil@gmail.com>
2021-01-27 13:04:50 +0300

    refactor: Code Review for Filters

--- src/components/filters/filters.js
+++ src/components/filters/filters.js
@@ -4,32 +4,31 @@ import './filters.css';
 export default class Filters extends Component {
 
   buttons = [
-    {name: `all`, label: `All`},
-    {name: `active`, label: `Active`},
-    {name: `done`, label: `Done`}
+    { name: `all`, label: `All`},
+    { name: `active`, label: `Active`},
+    { name: `done`, label: `Done`}
   ]
 
   render() {
 
-    const {filter, onFilterChange}=this.props;
+    const { filter, onFilterChange } = this.props;
 
     const buttons = this.buttons.map(({name, label}) => {
-      const isActive = filter === name;
 
-      const classNames = isActive ? `btn-outline-primary` : `btn-outline-secondary`;
+      const isActive = filter === name;
+      const classNames = `btn ${(isActive ? `btn-outline-primary` : `btn-outline-secondary`)}`;
 
       return (
-        <button className={`btn ${classNames}`}
-              type="button"
-              key={name}
-              onClick={() => onFilterChange(name)}>
-          {label}
+        <button className = { classNames }
+              type = "button" key = { name }
+              onClick = {() => onFilterChange(name)}>
+          { label }
         </button>)
     });
 
     return (
       <div>
-        {buttons}
+        { buttons }
       </div>
     );
   }
