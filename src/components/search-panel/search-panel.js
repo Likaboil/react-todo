c94f8b53afc319ebe48464a183138920912929de
Liliya Sagitova <guverboil@gmail.com>
2021-01-27 10:56:35 +0300

    feat: WP Adds state in SearchPanel

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -3,6 +3,10 @@ import './search-panel.css';
 
 export default class SearchPanel extends Component {
 
+  state={
+    label:``
+  }
+
   render() {
     return <input className="form-control search-input"
                   placeholder="search"
